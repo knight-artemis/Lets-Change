74800985c24297d2912c097ed1ffcd1a6ef775d0
BananaScript <150966690+BananaScript@users.noreply.github.com>
2024-03-06 00:53:52 +0300

    added time left calculations

--- client/src/pages/Main/Main.tsx
+++ client/src/pages/Main/Main.tsx
@@ -15,6 +15,39 @@ type ThingsType = {
 }
 // TODO добавить типизацию  категорий и массива категорий
 
+function getTimeLeft(endDate: Date): string {
+  const currentDate = new Date()
+  const targetDate = new Date(endDate)
+  const msDifference =  targetDate.getTime() - currentDate.getTime() 
+
+  const msInHour = 1000 * 60 * 60
+  const msInDay = msInHour * 24
+  const msInWeek = msInDay * 7
+  const msInMonth = msInDay * 30 // Приблизительно
+
+  let diffText
+
+  if (msDifference < msInDay) {
+    // Если разница меньше дня
+    const hoursDiff = Math.round(msDifference / msInHour)
+    diffText = `осталось ${hoursDiff} часов`
+  } else if (msDifference < msInWeek) {
+    // Если разница меньше недели
+    const daysDiff = Math.round(msDifference / msInDay)
+    diffText = `осталось ${daysDiff} дней`
+  } else if (msDifference < msInMonth) {
+    // Если разница меньше месяца
+    const weeksDiff = Math.round(msDifference / msInWeek)
+    diffText = `осталось ${weeksDiff} недель`
+  } else {
+    // Если разница больше месяца
+    const monthsDiff = Math.round(msDifference / msInMonth)
+    diffText = `осталось ${monthsDiff} месяцев`
+  } 
+
+  return diffText
+}
+
 export default function Main(): JSX.Element {
   const [things, setThings] = useState([])
   const [categories, setCategories] = useState([{ id: 0, categoryTitle: '' }])
@@ -38,25 +71,29 @@ export default function Main(): JSX.Element {
     <div className={style.wrapper}>
       <div className={style.sidebar}>
         {categories.map((cat) => (
-          <Button  key={cat.id}  link>
-            <div className={style.category}>
-              {cat.categoryTitle}
-            </div>
+          <Button key={cat.id} link>
+            <div className={style.category}>{cat.categoryTitle}</div>
           </Button>
         ))}
       </div>
       <div className={style.content}>
-        {things.map((thing:ThingsType) => (
-          <Button key={thing.id}  link>
+        {things.map((thing: ThingsType) => (
+          <Button key={thing.id} link>
             <div className={style.card}>
-              <div className={style.timeLeft}>{String(thing.endDate)}</div>
+              <div className={style.timeLeft}>{getTimeLeft(thing.endDate)}</div>
               <div className={style.photo}>
                 {/* <img src='http://localhost:3003/uploads/a.jpg' alt='фотка-шмотка'/> */}
-                <img src={thing.photoUrl || 'null'} alt='фотка-шмотка'/>
+                {/* Затычка */}
+                <img
+                  src='https://instrument.ru/img/dev/catalog_no_photo.png'
+                  alt='фотка-шмотка'
+                />
               </div>
-              <div className={style.name}>{thing.thingName}</div>
+              <div className={style.name}>
+                <center>{thing.thingName}</center>
+              </div>
               <div className={style.favorite}>хз</div>
-            </div>{' '}
+            </div>
           </Button>
         ))}
       </div>
