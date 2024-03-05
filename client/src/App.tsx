da358ba39dbf68563a85404d6d62ca677db72b12
knight-artemis <sobakar.artyom@gmail.com>
2024-03-05 13:12:36 +0300

    routes structure started + db locations deleted

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
 import './App.css'
+import Header from './components/Header/Header'
+import Footer from './components/Footer/Footer'
+import Main from './components/Main/Main'
 
 function App(): JSX.Element {
-  return <main></main>
+  return (
+    <>
+      <Header />
+      <Main/>
+      <Footer/>
+    </>
+  )
 }
 
 export default App
