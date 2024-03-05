da8d19bc4ae1f11fe0262381eed598dd9008df7c
Yarik Utkin <kryakkva@ya.ru>
2024-03-05 18:12:10 +0300

    navbar and routes

---
+++ client/src/pages/MyThings/MyThings.tsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import styles from './MyThings.module.css'
+
+export default function MyThings(): JSX.Element {
+  return (
+	<div className={styles.main}>MyThings</div>
+  )
+}
