6ac3061b9f9393c644210e23dc1dfff6cc33ffd4
Yarik Utkin <kryakkva@ya.ru>
2024-03-05 23:02:49 +0300

    addMap in progress

--- client/src/pages/MyDeals/MyDeals.tsx
+++ client/src/pages/MyDeals/MyDeals.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import styles from './MyDeals.module.css'
 
+
 export default function MyDeals(): JSX.Element {
   return (
 	<div className={styles.main}>MyDeals</div>
--- client/src/pages/NewThing/NewThing.tsx
+++ client/src/pages/NewThing/NewThing.tsx
@@ -1,8 +1,56 @@
-import React from 'react'
-import styles from './NewThing.module.css'
+import React, { useState } from 'react';
+import { Map, Placemark } from '@pbe/react-yandex-maps';
+import styles from './NewThing.module.css';
+import Button from '../../components/Controls/Button/Button';
+import useGeoLocation from '../../hooks/useGeoLocation';
 
-export default function NewThing():JSX.Element {
+export default function NewThing(): JSX.Element {
+  const location = useGeoLocation();
+  // console.log(typeof location.pos?.latitude)
+
+  const [pinCoords, setPinCoords] = useState([
+    location.pos?.latitude,
+    location.pos?.longitude,
+  ]);
+  // console.log(plm);
+  const handleClick = (e): void => {
+    console.log(e);
+    const coords = e.get('coords');
+    console.log('Координаты клика:', coords);
+    setPinCoords(coords);
+  };
+
   return (
-	<div className={styles.main}>NewThing</div>
-  )
+    <div className={styles.main}>
+      <h1>Добавить вещь</h1>
+      <h5>Добавьте название</h5>
+      <h5>Добавьте описание</h5>
+      <h5>Выберите категорию</h5>
+      <h5>Выберите длительность размещения</h5>
+      <h5>Выберите фото</h5>
+      <h5>Выберите локацию</h5>
+      <div>
+        <Map
+          onClick={(e) => handleClick(e)}
+          width='500px'
+          height='300px'
+          defaultState={{
+            center: [location.pos?.latitude, location.pos?.longitude],
+            zoom: 15,
+            controls: ['zoomControl', 'fullscreenControl'],
+          }}
+        >
+          <Placemark
+            onClick={() => console.log('click')}
+            defaultGeometry={[location.pos?.latitude, location.pos?.longitude]}
+            properties={{
+              balloonContentBody:
+                'This is balloon loaded by the Yandex.Maps API module system',
+            }}
+          />
+        </Map>
+      </div>
+      <Button>Загрузить</Button>
+    </div>
+  );
 }
