1842e6e5ecb6c76e495cfa8178e626703241b616
BananaScript <150966690+BananaScript@users.noreply.github.com>
2024-03-07 00:25:23 +0300

    my things added no things behavior

--- client/src/pages/MyThings/MyThings.tsx
+++ client/src/pages/MyThings/MyThings.tsx
@@ -5,6 +5,7 @@ import type { SimplifiedThingType } from '../../types'
 import { useAppSelector } from '../../redux/hooks'
 import Card from '../../components/Widgets/Card/Card'
 import style from './MyThings.module.css'
+import Button from '../../components/Controls/Button/Button'
 
 const ThingInitVal = {
   id: 0,
@@ -36,9 +37,33 @@ export default function MyThings(): JSX.Element {
 
   return (
     <div className={style.content}>
-      {things.map((thing: SimplifiedThingType) => (
-        <Card thing={thing} />
-      ))}
+      {things.length !== 0 ? (
+        things.map((thing: SimplifiedThingType) => <Card thing={thing} />)
+      ) : (
+        <div className={style.emptyPage}>
+          <span>Вы ещё не добавили никаких вещей</span>
+          <Button link onClick={() => void navigate(`/new-thing`)}>
+            <div className={style.svgLink}>
+              <img
+                className={style.icon}
+                src='assets/icons/add-thing.svg'
+                alt='svg'
+              />
+              <div>Добавить свою вещь для обмена</div>
+            </div>
+          </Button>
+          <Button link onClick={() => void navigate(`/`)}>
+            <div className={style.svgLink}>
+              <img
+                className={style.icon}
+                src='assets/icons/search-large.svg'
+                alt='svg'
+              />
+              <div>Посмотреть вещи других пользователей</div>
+            </div>
+          </Button>
+        </div>
+      )}
     </div>
   )
 }
