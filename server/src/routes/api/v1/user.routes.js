9feade3629dbf9d5ac5800d55345ce6b0561e6d5
knight-artemis <sobakar.artyom@gmail.com>
2024-03-07 20:50:40 +0300

    user routers in progress

--- server/src/routes/api/v1/user.routes.js
+++ server/src/routes/api/v1/user.routes.js
@@ -1,23 +1,36 @@
 const router = require('express').Router()
 
-router.put('/avatarUpload', async (req, res) => {
+const bcrypt = require('bcrypt');
+const { User } = require('../../../../db/models')
 
-})
+// router.put('/avatarUpload', async (req, res) => {
 
-router.put('/initialsUpd', async (req, res) => {
+// })
 
-})
+// router.put('/initialsUpd', async (req, res) => {
 
-router.put('/emailUpd', async (req, res) => {
+// })
 
-})
+// router.put('/emailUpd', async (req, res) => {
 
-router.put('/addressUpd', async (req, res) => {
+// })
 
-})
+//! ручка с адресом на холде
+
+// router.put('/addressUpd', async (req, res) => {
+
+// })
 
 router.put('/passUpd', async (req, res) => {
-
+  const { oldPassword, newPassword } = req.body
+  const { user } = req.session
+  const reqUser = await User.findOne({ where: { id: user.id } })
+  const finUser = reqUser.get({ plain: true })
+  if (await bcrypt.compare(oldPassword, finUser.password)) {
+    console.log('сравнение прошло успешно');
+  } else {
+    console.log('Что-то пошло не так');
+  }
 })
 
 module.exports = router
