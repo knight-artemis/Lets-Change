fe64da002adc497202aab88cdce0112c52ed29b6
BananaScript <150966690+BananaScript@users.noreply.github.com>
2024-03-11 00:56:23 +0300

    fixed my his thing on chat page

--- client/src/pages/Deal/Deal.tsx
+++ client/src/pages/Deal/Deal.tsx
@@ -54,9 +54,9 @@ export default function Deal(): JSX.Element {
       <div className={style.left}>
         <div className={style.thing}>
           <div className={style.text}>Твою вещь</div>
-          <CardSimple hoverable thing={deal?.Thing} />
+          <CardSimple hoverable thing={deal && deal.initiatorId === user.id ? deal.initiatorThings[0] : deal.Thing} />
           <div className={style.text}>меняют на</div>
-          <CardSimple hoverable thing={deal?.initiatorThings[0]} />
+          <CardSimple hoverable thing={deal && deal.initiatorId === user.id ? deal.Thing : deal.initiatorThings[0]} />
         </div>
         <Button color='good'>Сделка завершена</Button>
       </div>
