9184f238853ae53f421f029abbd535be14989670
BananaScript <150966690+BananaScript@users.noreply.github.com>
2024-03-11 01:00:11 +0300

    fixed my his thing... again

--- client/src/pages/Deal/Deal.tsx
+++ client/src/pages/Deal/Deal.tsx
@@ -53,10 +53,12 @@ export default function Deal(): JSX.Element {
     <div className={style.wrapper}>
       <div className={style.left}>
         <div className={style.thing}>
-          <div className={style.text}>Твою вещь</div>
-          <CardSimple hoverable thing={deal && deal.initiatorId === user.id ? deal.initiatorThings[0] : deal.Thing} />
-          <div className={style.text}>меняют на</div>
-          <CardSimple hoverable thing={deal && deal.initiatorId === user.id ? deal.Thing : deal.initiatorThings[0]} />
+          <div className={style.text}>{deal && deal.initiatorId === user.id ? 'За эту вещь' : 'Твою вещь'}</div>
+          <CardSimple hoverable thing={deal && deal.Thing} />
+          {/* <CardSimple hoverable thing={deal && deal.initiatorId === user.id ? deal.initiatorThings[0] : deal.Thing} /> */}
+          <div className={style.text}>{deal && deal.initiatorId === user.id ? 'ты предлагаешь' : 'меняют на'}</div>
+          <CardSimple hoverable thing={deal && deal.initiatorThings[0]} />
+          {/* <CardSimple hoverable thing={deal && deal.initiatorId === user.id ? deal.Thing : deal.initiatorThings[0]} /> */}
         </div>
         <Button color='good'>Сделка завершена</Button>
       </div>
