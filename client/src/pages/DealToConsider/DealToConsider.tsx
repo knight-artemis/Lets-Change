92906d987a08ee5d72d790accdd68b66c7ba1ded
BananaScript <150966690+BananaScript@users.noreply.github.com>
2024-03-12 01:53:51 +0300

    dealtoconsider started

--- client/src/pages/DealToConsider/DealToConsider.tsx
+++ client/src/pages/DealToConsider/DealToConsider.tsx
@@ -8,6 +8,11 @@ import Button from '../../components/Shared/Button/Button'
 import CardSimple from '../../components/Widgets/CardSimple/CardSimple'
 import { useAppDispatch } from '../../redux/hooks'
 import { fetchGetNot } from '../../redux/user/userThunkActions'
+import WholePage from '../../components/PageSkeleton/WholePage/WholePage'
+import SideBar from '../../components/PageSkeleton/SideBar/SideBar'
+import SvgLink from '../../components/Shared/SvgLink/SvgLink'
+import MainContent from '../../components/PageSkeleton/MainContent/MainContent'
+import Grid from '../../components/PageSkeleton/Grid/Grid'
 
 export default function DealToConsider(): JSX.Element {
   const [deal, setDeal] = useState<OneDealDetailed>()
@@ -37,9 +42,6 @@ export default function DealToConsider(): JSX.Element {
       { withCredentials: true },
     )
     navigate('/my-deals/from-me')
-
-    // тут запрос в бд и подтверждение сделки
-    // затем навигейт на страницу сделки
   }
   const rejectedHandler = async (): Promise<void> => {
     // тут пишемручку на отказ. мб
@@ -49,8 +51,6 @@ export default function DealToConsider(): JSX.Element {
       { withCredentials: true },
     )
     navigate('/my-deals/from-me')
-    // тут запрос в бд и отказ от сделки
-    // затем навигейт на страницу всех своих сделок
   }
 
   const selectorHandler = (thingId: number): void => {
@@ -62,6 +62,69 @@ export default function DealToConsider(): JSX.Element {
   // }
 
   return (
+    <WholePage>
+      <SideBar center>
+        <div className={style.textCol}>Моя вещь:</div>
+        <div className={style.topLine}>
+          <div className={style.oneThing}>
+            <CardSimple hoverable thing={deal?.Thing} thingId={deal?.thingId} />
+          </div>
+        </div>
+      </SideBar>
+      <MainContent>
+        <Grid>
+          {deal?.initiatorThings.map((hisOneThing) => (
+            <div key={hisOneThing.id} className={style.oneThing}>
+              <input
+                // style={{ display: 'none' }}
+                className={style.checkbox}
+                type='radio'
+                id={`thing-${hisOneThing.id}`}
+                name='selectedThing'
+                checked={selectedThingId === hisOneThing.id}
+              />
+
+              <label
+                htmlFor={`thing-${hisOneThing.id}`}
+                className={style.radioLabel}
+              >
+                <div className={style.borderWrapper}>
+                  <CardSimple
+                    hoverable
+                    thing={hisOneThing}
+                    thingId={hisOneThing.id}
+                  />
+                </div>
+              </label>
+              <Button onClick={() => selectorHandler(hisOneThing.id)}>
+                выбрать
+              </Button>
+            </div>
+          ))}
+        </Grid>
+      
+      <div className={style.selectedText}>
+        Выбрано: {deal?.initiatorThings[selectedThingId]?.thingName}
+      </div>
+      <div className={style.bottomLine}>
+        <Button
+          disabled={selectedThingId < 0}
+          color='good'
+          onClick={() => void acceptedHandler()}
+        >
+          Давай меняться
+        </Button>
+        <Button color='danger' onClick={() => void rejectedHandler()}>
+          Не хочу меняться
+        </Button>
+      </div>
+      </MainContent>
+    </WholePage>
+  )
+}
+
+/*
+
     <div className={style.wrapper}>
       <div className={style.textCol}>Моя вещь:</div>
       <div className={style.topLine}>
@@ -89,7 +152,11 @@ export default function DealToConsider(): JSX.Element {
               className={style.radioLabel}
             >
               <div className={style.borderWrapper}>
-                <CardSimple hoverable thing={hisOneThing} thingId={hisOneThing.id} />
+                <CardSimple
+                  hoverable
+                  thing={hisOneThing}
+                  thingId={hisOneThing.id}
+                />
               </div>
             </label>
             <Button onClick={() => selectorHandler(hisOneThing.id)}>
@@ -113,6 +180,5 @@ export default function DealToConsider(): JSX.Element {
           Не хочу меняться
         </Button>
       </div>
-    </div>
-  )
-}
+    </div> 
+    */
