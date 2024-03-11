648b4d64c59c3df463e3eea4d9cc5bff5831ca6e
BananaScript <150966690+BananaScript@users.noreply.github.com>
2024-03-12 00:25:02 +0300

    grid comp

---
+++ client/src/components/PageSkeleton/Grid/Grid.tsx
@@ -0,0 +1,18 @@
+import type { ReactNode } from 'react'
+import React from 'react'
+import clsx from 'clsx'
+import style from './Button.module.css'
+
+type GridProps = {
+  children: ReactNode 
+//   onClick?: (e) => void 
+//   link?: boolean // кнопка-ссылка или обычная кнопка
+//   disabled?: boolean // заблочить
+//   color?: 'neutral' | 'danger' | 'good' | 'warning' | 'gray'| undefined // цвет
+}
+
+export default function Grid({children}:GridProps):JSX.Element {
+  return (
+    <div className={style.grid}>{children}</div>
+  )
+}
